21cd235e7c1f1713618c144557c5519a9f76b690
Emma Forner <emmafornermarques@gmail.com>
2019-01-21 00:10:06 +0100

    Small changes

--- ChartIMDB.py
+++ ChartIMDB.py
@@ -1,5 +1,4 @@
 from requests_html import HTMLSession
-import numpy.core._dtype_ctypes #this import is necessary only because of a current bug in pyinstaller
 import matplotlib.pyplot as plt
 
 # URL format: https://www.imdb.com/title/tt0235918/
@@ -29,25 +28,25 @@ except IndexError:
 web = session.get(series + "episodes")
 numberOfSeasons = web.html.find('[selected=selected]')[0].text
 if "Unknown" in numberOfSeasons:
+    # If the season selected is "Unknown", get all values in the bySeason selector
+    # and get the max value
     bySeasonOptions = web.html.find('#bySeason')[0].find('option')
     seasons = []
     for i in range(0, len(bySeasonOptions)):
         try:
             seasons.append(int(bySeasonOptions[i].text))
-        except ValueError:
+        except ValueError: # Is not a number: "Unknown"
             continue
     numberOfSeasons = max(seasons)
 numberOfSeasons = int(numberOfSeasons)
 
 # Loops trough seasons, adding the value of each episode:
+warningShowed = False # Handles the season skipped warning
 for i in range(0, numberOfSeasons):
-    if numberOfSeasons == 1:
-        web = session.get(series + "episodes?season=1")
-    else:
-        web = session.get(series + "episodes?season=" + str(i + 1))
+    web = session.get(series + "episodes?season=" + str(i + 1))
     # The problem of getting the rates with the class .ipl-rating-star__rating is that many
     # elements use that class. We have to remove non-numeric values first and then those who
-    # don't belong (numbers 1 to 10 between scores)
+    # don't belong (numbers 0 to 10 between scores)
     ratings = web.html.find('.ipl-rating-star__rating')
     new_ratings = []
     # Removes non-numeric values (I think all of them are "Rate")
@@ -63,10 +62,20 @@ for i in range(0, numberOfSeasons):
     # There's the numbers 1-10 between each score, we count to 10 and then add the following value
     n = 0
     for k in range(0, len(ratings)):
-        # Unrated episodes get the following value of zero.
-        # All the following episodes will be unrated, so we break here.
         if n == 0:
+            # Unrated episodes get the following value of zero.
+            # All the following episodes will be unrated, so we break here.
+            # THIS IS NOT TRUE FOR NOT SO POPULAR SERIES, because they may have unrated episodes
+            # The problem is that you can't make a reliable chart if some episodes are unrated
+            # so I'm not going to change anything here.
             if ratings[k] == 0:
+                if not warningShowed:
+                    print("Season skipped! It might be an unreleased season, or there might be an unrated episode.")
+                    print("If it's a single chapter, all the season has been skipped!")
+                    print("Charts like this with unrated chapters might not be reliable, consider another series, program, or presentation.")
+                    warningShowed = True
+                else:
+                    print("Season skipped!")
                 break
             new_ratings.append(ratings[k])
             n += 1
